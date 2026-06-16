78ea1f305abfb4b70d4f2b8bf7183b90fc2dfb0a
Mohd Kaif <kaifali200407@gmail.com>
2026-06-16 13:03:29 +0530

    Update project

--- warehouse_project/settings.py
+++ warehouse_project/settings.py
@@ -122,4 +122,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = '/home/MohdKaifashraf/warehouse_box-recommender/staticfiles'
